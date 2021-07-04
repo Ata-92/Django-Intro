be3dd585ac11f7b9f9e80103d946e6363c3c7df5
Ata-92 <atakal92@gmail.com>
2021-07-04 20:45:28 +0300

    Add one to one two models

--- src/dj_relations/models.py
+++ src/dj_relations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Creator(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
+class Languages(models.Model):
+    creator = models.OneToOneField(Creator, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
