75e11c29f59c3105296d9a38a6c4e48a5abb9e10
Ata-92 <atakal92@gmail.com>
2021-07-08 17:33:40 +0300

    Change home page url path

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, student_num
 
 urlpatterns = [
-    path("fs/", index, name="index"),
+    path("", index, name="index"),
     path('num/', student_num, name='student_num'),
 ]
