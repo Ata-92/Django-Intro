f5f61be4422785dbcebb085f0eaf51f8617bc067
Ata-92 <atakal92@gmail.com>
2021-07-08 17:04:47 +0300

    Add a new url

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, student_num
 
 urlpatterns = [
-    path("fs/", index, name="index")
+    path("fs/", index, name="index"),
+    path('num/', student_num, name='student_num'),
 ]
