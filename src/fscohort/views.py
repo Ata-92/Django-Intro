a3f6e7c783640c9b15abf34fbfa2ad75e83bb4b7
Ata-92 <atakal92@gmail.com>
2021-07-08 17:02:49 +0300

    Add a new view

--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from fscohort.models import Student
+
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello world!</h1>")
+
+def student_num(request):
+    num_of_stdnt = Student.objects.count()
+    return HttpResponse("FS Cohort has {} students". format(num_of_stdnt))
