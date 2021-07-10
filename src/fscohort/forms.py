ca015792df0ea8370c8a16989740aa0ab32e7f04
Ata-92 <atakal92@gmail.com>
2021-07-10 16:07:55 +0300

    Change Form class with ModelForm class

--- src/fscohort/forms.py
+++ src/fscohort/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
-class StudentForm(forms.Form):
-    first_name = forms.CharField(max_length=100, label="Your Name")
-    last_name = forms.CharField(max_length=100, label="Your Surname")
-    number = forms.IntegerField(required=False)
+from fscohort.models import Student
+
+# class StudentForm(forms.Form):
+#     first_name = forms.CharField(max_length=100, label="Your Name")
+#     last_name = forms.CharField(max_length=100, label="Your Surname")
+#     number = forms.IntegerField(required=False)
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["first_name", "last_name", "number"]
